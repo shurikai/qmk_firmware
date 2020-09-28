4e0850dae6f23bed5cc71709e0303bc5eaa36e80
jason <jason.chestnut@gmail.com>
2020-09-28 15:09:41 -0400

    Update user space configuration with RGB layer, and update some keymaps with newest user space.

--- users/shurikai/config.h
+++ users/shurikai/config.h
@@ -4,3 +4,15 @@
 #define COMBO_COUNT 1
 #define TAPPING_TERM 150
 #define IGNORE_MOD_TAP_INTERRUPT
+
+#ifdef RGBLIGHT_ENABLE
+#   define RGBLIGHT_SLEEP
+#   define RBG_HUE_STEP     16
+#   define RGB_SAT_STEP     16
+#   define RGB_VAL_STEP     16
+
+#   define RBGLIGHT_EFFECT_BREATHING
+#   define RBGLIGHT_EFFECT_KNIGHT
+#   define RBGLIGHT_EFFECT_SNAKE
+#   define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#endif

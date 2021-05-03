ab786f76bc771c0b896052acaf8b6078ade67d98
Jason Chestnut <jason.chestnut@gmail.com>
2021-05-03 11:47:01 -0400

    Remove unnecessary lines from rules.mk.

--- keyboards/pauper/pixie/rules.mk
+++ keyboards/pauper/pixie/rules.mk
@@ -7,16 +7,14 @@ MCU = STM32F072
 # EXTRAFLAGS+=-flto
 LTO_ENABLE = yes
 BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = lite  # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no # Mouse keys
-EXTRAKEY_ENABLE = yes # Audio control and System control
-CONSOLE_ENABLE = no  # Console for debug
-COMMAND_ENABLE = no    # Commands for debug and configuration
-SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
-NKRO_ENABLE = yes     # USB Nkey Rollover
-AUDIO_ENABLE = no
-RGBLIGHT_ENABLE = no
-SERIAL_LINK_ENABLE = no
+BOOTMAGIC_ENABLE = lite # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no    # Mouse keys
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+CONSOLE_ENABLE = no     # Console for debug
+COMMAND_ENABLE = no     # Commands for debug and configuration
+SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
+NKRO_ENABLE = yes       # USB Nkey Rollover
+RGBLIGHT_ENABLE = no    # RGB lighting
 
 
 # Enter lower-power sleep mode when on the ChibiOS idle thread
